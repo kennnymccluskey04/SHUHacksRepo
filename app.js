f163ef93cf98a1061f424b4b2c130de7f4eebe39
Kendall McCluskey <kennymccluskey04@gmail.com>
2024-04-10 19:43:30 -0400

    Added homepage

---
+++ app.js
@@ -0,0 +1,15 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username === "admin") {
+        // Redirect to a separate login page
+        window.location.href = "homepage.html";
+    } else {
+        // Perform regular login process
+        // For example, you can send a request to a server for authentication
+        console.log("Regular login process");
+    }
+});
